3a8a8cb0e2adb978053703440bd7c17eb31f4db3
L0615T1C5-216AC-9437 <60281819+L0615T1C5-216AC-9437@users.noreply.github.com>
2020-02-29 14:55:34 -0500

    Add files via upload

---
+++ src/main/java/AFK/Main.java
@@ -0,0 +1,58 @@
+package AFK;
+
+//-----imports-----//
+import arc.Events;
+import arc.util.Log;
+import mindustry.core.NetServer;
+import mindustry.entities.type.Player;
+import mindustry.game.EventType;
+import mindustry.gen.Call;
+import mindustry.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import static mindustry.Vars.*;
+
+
+public class Main extends Plugin {
+    private boolean aks = false;
+    public static HashMap<String, pp> PlayerPos = new HashMap<>();
+
+    public Main() throws InterruptedException {
+        Thread AS = new Thread() {
+            public void run() {
+                Log.info("AFK started Successfully!");
+                while (aks) {
+                    for (Player p : playerGroup.all()) {
+                        if (p.x / 8 == PlayerPos.get(p.uuid).getX() && p.y / 8 == PlayerPos.get(p.uuid).getY()) {
+                            p.con.kick("Connection Closed for being AFK",1);
+                        } else {
+                            PlayerPos.get(p.uuid).setX(p.x / 8);
+                            PlayerPos.get(p.uuid).setY(p.y / 8);
+                        }
+                    }
+                    try {
+                        TimeUnit.SECONDS.sleep(5 * 60);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        aks = true;
+        AS.start();
+        Log.info("Attempting to start AFK...");
+
+        Events.on(EventType.PlayerJoin.class, event -> {
+            Player player = event.player;
+            PlayerPos.put(player.uuid, new pp());
+            PlayerPos.get(player.uuid).setX(0);
+            PlayerPos.get(player.uuid).setY(0);
+        });
+        Events.on(EventType.PlayerLeave.class, event -> {
+            Player player = event.player;
+            PlayerPos.remove(player.uuid);
+        });
+    }
+}
---
+++ src/main/java/AFK/pp.java
@@ -0,0 +1,19 @@
+package AFK;
+
+public class pp {
+    //Info
+    private float x;
+    private float y;
+
+    public pp() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    //modify
+    public void setX(float x) {this.x = x;}
+    public void setY(float y) {this.y = y;}
+    //get
+    public float getX() {return x;}
+    public float getY() {return y;}
+}
